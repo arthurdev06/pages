4a35ce29765dc09eea10a416d566d4ce2b0a406b
Arthur Rocha <arthurdev6@gmail.com>
2024-02-29 10:19:32 -0300

    fix timer

--- mini-projects/timer/script.js
+++ mini-projects/timer/script.js
@@ -1,48 +1,49 @@
+var sec = 0;
+var min = 0;
+var hr = 0;
+var interval;
+var isTimerRunning = false;
 
-var sec=0
-var min=0
-var hr=0
+function twoDigits(digit) {
+  if (digit < 10) {
+    return "0" + digit;
+  } else {
+    return digit;
+  }
+}
 
-var interval
+function start() {
+  if (!isTimerRunning) {
+    watch();
+    interval = setInterval(watch, 1000);
+    isTimerRunning = true;
+  }
+}
 
-function twoDigits(digit){
-    if(digit<10){
-        return('0'+digit)
-    }else{
-        return(digit)
+function pause() {
+  clearInterval(interval);
+  isTimerRunning = false;
+}
+
+function stop() {
+  clearInterval(interval);
+  sec = 0;
+  min = 0;
+  hr = 0;
+  isTimerRunning = false;
+  document.getElementById("watch").innerText = "00:00:00";
+}
+
+function watch() {
+  sec++;
+  if (sec == 60) {
+    min++;
+    sec = 0;
+    if (min == 60) {
+      min = 0;
+      hr++;
     }
-}
-
-function start(){
-    watch()
-    interval= setInterval(watch,1000)
-
-
-}
-
-function pause(){
-    clearInterval(interval)
-
-}
-
-function stop(){
-    clearInterval(interval)
-    sec=0
-    min=0
-    window.alert("You've stopped at: "+document.getElementById('watch').innerText)
-    document.getElementById('watch').innerText='00:00:00'
-
-}
-
-function watch(){
-    sec++
-    if(sec==60){
-        min++
-        sec=0
-        if(min==60){
-            min=0
-            hr++
-        }
-    }
-    document.getElementById('watch').innerText=twoDigits(hr)+':'+twoDigits(min)+':'+twoDigits(sec)
+  }
+  document.getElementById("watch").innerText =
+    twoDigits(hr) + ":" + twoDigits(min) + ":" + twoDigits(sec);
 }
